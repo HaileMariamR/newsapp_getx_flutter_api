6a4ee038170b45021f1ee68eda265aa4f71cab91
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-27 20:21:30 +0300

    working on tabbar

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -1,55 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:newsapp/utilities/constants.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+  Homepage({Key? key}) : super(key: key);
+  List<Widget> listoftabstitle = [
+    Text(
+      "Home",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Business",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Entertainment",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "General",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Health",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Sports",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Technology",
+      style: ktabtitlestyle,
+    ),
+    Text(
+      "Science",
+      style: ktabtitlestyle,
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        drawer: Drawer(
-            child: Column(
-          children: [
-            DrawerHeader(
-              child: Container(),
-            ),
-          ],
-        )),
-        appBar: AppBar(
-          title: Container(
-            padding: EdgeInsets.only(right: 30),
-            width: 300,
-            height: 35,
-            child: TextField(
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.search),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    borderSide: BorderSide(color: Colors.black, width: 1)),
-                hintText: "Search news ...",
-                labelStyle: TextStyle(color: Colors.black),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    borderSide: BorderSide(color: Colors.black, width: 1)),
-                hintStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
+      child: DefaultTabController(
+        length: listoftabstitle.length,
+        child: Scaffold(
+          drawer: Drawer(
+              child: Column(
+            children: [
+              DrawerHeader(
+                child: Container(),
+              ),
+            ],
+          )),
+          appBar: AppBar(
+            title: Container(
+              padding: EdgeInsets.only(right: 30),
+              width: 300,
+              height: 30,
+              child: TextField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.search),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(color: Colors.black, width: 1)),
+                  hintText: "Search news ...",
+                  labelStyle: TextStyle(color: Colors.black),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(color: Colors.black, width: 1)),
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
+            actions: [
+              Container(
+                  width: 20,
+                  margin: EdgeInsets.only(right: 10),
+                  child: TextButton(
+                      onPressed: () {}, child: Icon(Icons.notifications)))
+            ],
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black87),
+            backgroundColor: Colors.white70,
+            bottom: TabBar(
+              indicatorColor: Colors.green,
+              indicatorWeight: 4,
+              indicatorSize: TabBarIndicatorSize.label,
+              isScrollable: true,
+              tabs: [
+                for (var item in listoftabstitle)
+                  Tab(
+                    child: item,
+                  ),
+              ],
+            ),
           ),
-          actions: [
-            Container(
-                width: 20,
-                margin: EdgeInsets.only(right: 10),
-                child: TextButton(
-                    onPressed: () {}, child: Icon(Icons.notifications)))
-          ],
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black87),
-          backgroundColor: Colors.white70,
+          body: Body(),
         ),
-        body: Body(),
       ),
     );
   }
@@ -60,18 +110,15 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // child: TextField(
-        //   decoration: InputDecoration(
-        //       border: OutlineInputBorder(
-        //         borderSide: BorderSide(color: Colors.black, width: 10.0),
-        //       ),
-        //       hintText: "Search news ...",
-        //       focusedBorder: InputBorder.none,
-        //       hintStyle: TextStyle(
-        //         fontWeight: FontWeight.bold,
-        //       )),
-        // ),
-        );
+    return TabBarView(children: [
+      Text("1"),
+      Text("2"),
+      Text("3"),
+      Text("4"),
+      Text("5"),
+      Text("6"),
+      Text("7"),
+      Text("8"),
+    ]);
   }
 }
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const ktabtitlestyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Benne',
+);
