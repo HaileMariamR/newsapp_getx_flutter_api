aec0a28a40e9ef08b03fd9fb872066b2d30da4a2
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-29 20:03:23 +0300

    implement share functionality

--- lib/screens/Categories/Home.dart
+++ lib/screens/Categories/Home.dart
@@ -3,6 +3,9 @@ import 'package:newsapp/appState/AppState.dart';
 import 'package:get/get.dart';
 import 'package:newsapp/utilities/constants.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:share_plus_linux/share_plus_linux.dart';
+import 'package:share_plus/share_plus.dart';
+
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,65 +15,64 @@ class Home extends StatelessWidget {
     AppState appstate = Get.find();
     return Container(
         margin: EdgeInsets.only(top: 10),
-        child: (appstate.isfetching == false)
-            ? SpinKitRing(
-                size: 50,
-                color: Colors.black38,
-              )
-            : Obx(
-                () => ListView.builder(
-                    itemCount: appstate.allNews.length,
-                    itemBuilder: (context, int index) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height / 2,
-                        margin: EdgeInsets.all(2),
-                        child: Card(
-                          elevation: 4,
-                          child: Column(children: [
-                            Container(
-                              child: Expanded(
-                                child: Image.network(
-                                  (appstate.allNews[index]['urlToImage'] !=
-                                          null)
-                                      ? appstate.allNews[index]['urlToImage']
-                                      : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROGVlwDhbC-6RixbdgEwDrABJ6BD3hhM2eJA&usqp=CAU",
-                                  errorBuilder: (context, Object exception,
-                                      StackTrace? stackTrace) {
-                                    return const Text('Sorry Image not found ');
+        child: Obx(
+          () => ListView.builder(
+              itemCount: appstate.allNews.length,
+              itemBuilder: (context, int index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  margin: EdgeInsets.all(2),
+                  child: Card(
+                    elevation: 4,
+                    child: Column(children: [
+                      Container(
+                        child: Expanded(
+                          child: Image.network(
+                            (appstate.allNews[index]['urlToImage'] != null)
+                                ? appstate.allNews[index]['urlToImage']
+                                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROGVlwDhbC-6RixbdgEwDrABJ6BD3hhM2eJA&usqp=CAU",
+                            errorBuilder: (context, Object exception,
+                                StackTrace? stackTrace) {
+                              return const Text('Sorry Image not found ');
+                            },
+                          ),
+                        ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: Text(
+                            "${appstate.allNews[index]['title']}",
+                            style: karticletitlestyle,
+                          )),
+                      Container(
+                          margin: EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Share.share(
+                                        "${appstate.allNews[index]['url']}");
                                   },
+                                  child: Icon(Icons.share),
                                 ),
                               ),
-                            ),
-                            Container(
-                                margin: EdgeInsets.only(top: 10),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                margin: EdgeInsets.only(left: 140),
                                 child: Text(
-                                  "${appstate.allNews[index]['title']}",
-                                  style: karticletitlestyle,
-                                )),
-                            Container(
-                                margin: EdgeInsets.all(10),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Container(
-                                      child: Icon(Icons.share),
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.all(5),
-                                      margin: EdgeInsets.only(left: 140),
-                                      child: Text(
-                                        "view detail",
-                                        style: TextStyle(color: Colors.blue),
-                                      ),
-                                    )
-                                  ],
-                                ))
-                          ]),
-                        ),
-                      );
-                    }),
-              ));
+                                  "view detail",
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              )
+                            ],
+                          ))
+                    ]),
+                  ),
+                );
+              }),
+        ));
   }
 }
